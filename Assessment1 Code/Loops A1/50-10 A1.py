2db24626cf6b84c7eff4556d488f0d5bc7da061c
BRRRT_Central <164846967+MilleteNowhere@users.noreply.github.com>
2024-10-24 11:04:52 +0400

    Add files via upload

---
+++ Assessment1 Code/Loops A1/50-10 A1.py
@@ -0,0 +1,2 @@
+for x in range(100, 205, 5):
+    print (x)
---
+++ Assessment1 Code/Simple Sums A1.py
@@ -0,0 +1,9 @@
+#Calls out two variable integers to be used in the "Sum" Var
+
+Var1 = 8
+Var2 = 10
+
+#Adds both Variables and combines them into a single variable
+Sum = Var1 + Var2
+
+print (Sum)
